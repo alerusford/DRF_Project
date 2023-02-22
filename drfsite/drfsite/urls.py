ec1c239a480e9f236f9adf07643c4359645e4268
alexandr <alerusford@gmail.com>
2023-02-22 18:52:26 +0300

    + JWT auth

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from women.views import *
 
@@ -28,4 +29,7 @@ urlpatterns = [
     path('api/v1/womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # JWT
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # JWT
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'), # JWT
 ]
