7e9dac0c0a5484c02ca5c94b08ee6afd76dcc388
alexandr <alerusford@gmail.com>
2023-02-22 15:18:34 +0300

    djoser auth

--- drfsite/drfsite/settings.py
+++ drfsite/drfsite/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'women.apps.WomenConfig',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser'
 ]
 
 MIDDLEWARE = [
@@ -135,4 +137,9 @@ REST_FRAMEWORK = {
     #     # AllowAny доступ для всех
     #     'rest_framework.permissions.IsAuthenticated',
     # ]
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,8 +1,9 @@
 from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from .models import Women, Category
 from .permissons import IsAdminOrReadOnly, IsOwnerOrReadOnly
@@ -14,10 +15,15 @@ class WomenAPIList(generics.ListCreateAPIView):
     serializer_class = WomenSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
 
+
+
+
 class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
-    permission_classes = (IsOwnerOrReadOnly, )
+    # permission_classes = (IsOwnerOrReadOnly, )
+    permission_classes = (IsAuthenticated, )
+    # authentication_classes = (TokenAuthentication, ) # класс аутентификации
 
 class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
     queryset = Women.objects.all()
